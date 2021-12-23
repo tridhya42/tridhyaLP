6e2a59d2430f4de3aa93762fb97383cb05e8c3e2
TRD039\Administrator <divyavtridhyatech@gmail.com>
2021-12-23 17:12:21 +0530

    Moved static files into backup folder

---
+++ backup_20211223/sendmail.php
@@ -0,0 +1,9 @@
+<?php
+
+// Settings
+$settings = array(
+    "name"          => "Tridhya Tech GmbH",
+    "email"         => "hello@tridhya.de",
+);
+
+require_once "phpmailer/contact_form.php";
--- sendmail.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Settings
-$settings = array(
-    "name"          => "Tridhya Tech GmbH",
-    "email"         => "hello@tridhya.de",
-);
-
-require_once "phpmailer/contact_form.php";
